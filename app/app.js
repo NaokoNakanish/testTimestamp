49ff429a2ea1b4c544569de8ad511dbb17ca2737
naokonakanish <fu2fuka.nao@gmail.com>
2021-11-01 00:48:09 +0900

    Package subpath './package.json' is not defined by "exports"

--- app/app.js
+++ app/app.js
@@ -1,7 +1,24 @@
-import Vue from 'nativescript-vue'
+import Vue from "nativescript-vue";
 
-import Home from './components/Home'
+import Home from "./components/Home";
+
+// Eddyさんの参照　https://github.com/EddyVerbruggen/nativescript-plugin-firebase#usage
+var firebase = require("@nativescript/firebase").firebase;
+
+firebase
+  .init({
+    // Optionally pass in properties for database, authentication and cloud messaging,
+    // see their respective docs.
+  })
+  .then(
+    function () {
+      console.log("firebase.init done");
+    },
+    function (error) {
+      console.log("firebase.init error: " + error);
+    }
+  );
 
 new Vue({
-  render: (h) => h('frame', [h(Home)]),
-}).$start()
+  render: (h) => h("frame", [h(Home)]),
+}).$start();
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const webpack = require("@nativescript/webpack");
 
 module.exports = (env) => {
-	webpack.init(env);
+  webpack.init(env);
 
-	// Learn how to customize:
-	// https://docs.nativescript.org/webpack
+  // Learn how to customize:
+  // https://docs.nativescript.org/webpack
+  webpack.useConfig("vue");
 
-	return webpack.resolveConfig();
+  return webpack.resolveConfig();
 };
-
-
